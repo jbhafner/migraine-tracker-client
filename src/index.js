12d08721848da3b142809357e599ad1361c0beac
Brian Hafner <bhafner@Darrens-MBP.localdomain>
2019-08-23 11:17:09 -0700

    registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker.js";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 registerServiceWorker();
